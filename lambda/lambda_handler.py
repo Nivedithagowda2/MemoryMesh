e7e0afd1deb17aa9f0c88687a5fdd51bed23f428
Niveditha . <nivedithar127@gmail.com>
2026-09-23 22:38:20 +0530

    Fix formatting in lambda_handler.py instructions

--- lambda/lambda_handler.py
+++ lambda/lambda_handler.py
@@ -7,7 +7,7 @@ Deploy notes:
   3. cp -r app package/
   4. cp lambda/lambda_handler.py package/     
   5. cd package && zip -r ../memorymesh_lambda.zip . && cd ..    
-  6. Create Lambda function (Python 3.12), upload memorymesh_lambda.zip 
+  6. Create Lambda function (Python 3.12), upload memorymesh_lambda.zip  
   7. Handler: lambda_handler.handler
   8. Set env vars (DATABASE_URL, AWS_REGION, USE_BEDROCK, etc.)         
   9. Enable a Function URL             
